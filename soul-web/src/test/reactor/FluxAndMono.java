1ac6b1f809098bdb9f5ed1540446efb29c3b5a52
spring-mac <zhangxiuchun128@163.com>
2019-04-19 08:32:46 +0800

    添加hutool maven依赖

---
+++ soul-web/src/test/reactor/FluxAndMono.java
@@ -0,0 +1,26 @@
+package reactor;
+
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+public class FluxAndMono {
+
+    public void test01() throws Exception {
+        Flux.create(sink -> {
+            sink.next("sink：" + Thread.currentThread().getName());
+            sink.complete();
+        })
+                .publishOn(Schedulers.single())
+                .map(x -> x + "   map：" + Thread.currentThread().getName())
+                .subscribeOn(Schedulers.parallel())
+                .toStream()
+                .forEach(System.out::println);
+
+
+    }
+
+    public static void main(String[] args) throws Exception {
+        FluxAndMono fluxAndMono = new FluxAndMono();
+        fluxAndMono.test01();
+    }
+}
